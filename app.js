2802faa2734ca5aa7b29a9125a9185fbe45fdaef
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-09-20 22:42:05 +0300

    feat: добавить миддлвэр

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const userRouter = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 
@@ -9,18 +10,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-
 app.use((req, res, next) => {
   req.user = {
     _id: '5d8b8592978f8bd833ca8133',
   };
 
   next();
+});
+
+app.use(userRouter);
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.listen(PORT);
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { getUsers, getUsersById, createUser } = require('../controllers/users');
 
 router.get('/users', getUsers);
-router.get('/users/:userId', getUsersById)
+router.get('/users/:userId', getUsersById);
 router.post('/users', createUser);
 
 module.exports = router;
