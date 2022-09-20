1323743b6d65531cdd3de448466e35b818b5c98c
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-09-20 22:17:04 +0300

    feat: добавить роут users

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { getUsers, getUsersById, createUser } = require('../controllers/users');
+
+router.get('/users', getUsers);
+router.get('/users/:userId', getUsersById)
+router.post('/users', createUser);
+
+module.exports = router;
