2162c02a278b977bb862083017867fd51e7b9bc1
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-10-05 22:47:38 +0300

    feat: добавить модели email и password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,6 +17,21 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(email) {
+        return isEmail(email);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
   },
 }, { versionKey: false });
 
