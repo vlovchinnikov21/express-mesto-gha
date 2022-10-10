59695c2271507815c715712b8e3f4a099e75b020
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-10-10 17:42:58 +0300

    fix: устранить зависание запроса

--- controllers/users.js
+++ controllers/users.js
@@ -31,9 +31,17 @@ module.exports.createUser = (req, res, next) => {
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send({
+      data: {
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        email: user.email,
+        password: user.password,
+      },
+    }))
     .catch((err) => {
-      if (err.name === 'MongoError' && err.code === 11000) {
+      if (err.code === 11000) {
         throw new ConflictError('Пользователь с таким email уже существует');
       }
     })
