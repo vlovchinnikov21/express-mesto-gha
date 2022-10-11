1ae69249a2c51a6e3ea657a15a0d6d6b180eba72
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-10-11 20:07:33 +0300

    fix: исправить краш сервера

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const { login, createUser } = require('./controllers/users');
 const { auth } = require('./middlewars/auth');
 const NotFoundError = require('./error-codes/NotFoundError');
 const { userValidation, loginValidation } = require('./middlewars/validation');
-const { checkServer } = require('./controllers/checkServer');
 
 const { PORT = 3000 } = process.env;
 
@@ -33,6 +32,17 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(errors());
 
-app.use(checkServer());
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
 
 app.listen(PORT);
